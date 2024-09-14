39dda8acdd7831214a4691dbf91f2abc19e02f7d
YanaEgorova <45537884+YanaEgorova@users.noreply.github.com>
2024-09-14 03:26:17 +0300

    Initial commit

---
+++ src/components/First/First.jsx
@@ -0,0 +1,12 @@
+import clsx from "clsx";
+import { AiFillAccountBook } from "react-icons/ai";
+
+console.log(clsx);
+
+export default function First() {
+    return (
+        <div>
+            <AiFillAccountBook />
+        </div>
+    );
+};
